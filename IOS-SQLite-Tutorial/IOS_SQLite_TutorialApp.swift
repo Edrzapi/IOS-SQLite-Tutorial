efbf10febb26099761ceb767e4fbfc61d1c7a3a9
edrzapi <edrzapi@gmail.com>
2024-11-27 20:54:08 +0000

    Initial Commit

---
+++ IOS-SQLite-Tutorial/IOS_SQLite_TutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOS_SQLite_TutorialApp.swift
+//  IOS-SQLite-Tutorial
+//
+//  Created by Ed on 27/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct IOS_SQLite_TutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ IOS-SQLite-TutorialTests/IOS_SQLite_TutorialTests.swift
@@ -0,0 +1,17 @@
+//
+//  IOS_SQLite_TutorialTests.swift
+//  IOS-SQLite-TutorialTests
+//
+//  Created by Ed on 27/11/2024.
+//
+
+import Testing
+@testable import IOS_SQLite_Tutorial
+
+struct IOS_SQLite_TutorialTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
